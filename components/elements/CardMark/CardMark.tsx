d0437db77fcd53c1c2947044fe22b044b3656609
pkovalchuk <pavel.kovalchuk@dataart.com>
2022-11-04 16:56:58 +0100

    fix tags

--- components/elements/CardMark/CardMark.tsx
+++ components/elements/CardMark/CardMark.tsx
@@ -6,7 +6,7 @@ import { IPropsCardMark } from './model';
 const CardMark: FC<IPropsCardMark> = ({ mark, correctAnswer, allAnswer }) => {
   return (
     <>
-      <h4>{`Your mark is: <i>${mark}</i>.`}</h4>
+      <h4>{`Your mark is: ${mark}.`}</h4>
       <p>{`Correct ${correctAnswer} out of ${allAnswer}.`}</p>
     </>
   );
