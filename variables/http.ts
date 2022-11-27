fc7a174b7521744a8174123e71d12ead399465e6
pkovalchuk <pavel.kovalchuk@dataart.com>
2022-11-27 11:41:54 +0100

    test 1

--- variables/http.ts
+++ variables/http.ts
@@ -1,10 +1,10 @@
 export const HTTP_REQUEST_URL = {
-  PRONOUN: '/api/pronoun',
-  VERBS_GROUPS: '/api/verbs-groups',
-  VERBS: '/api/verbs',
-  VERBS_BY_GROUPS: '/api/verbs/by-groups',
-  VERBS_TRAININGS: '/api/verbs/trainings',
-  USER_TRAININGS: '/api/user/trainings',
+  PRONOUN: 'api/pronoun',
+  VERBS_GROUPS: 'api/verbs-groups',
+  VERBS: 'api/verbs',
+  VERBS_BY_GROUPS: 'api/verbs/by-groups',
+  VERBS_TRAININGS: 'api/verbs/trainings',
+  USER_TRAININGS: 'api/user/trainings',
 };
 
 export const URL_PARAMS = {
